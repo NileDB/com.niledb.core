a2ffeb7073189582e46d2a20689f3e43f55d548f
Paco Hernández Gómez <paco@hernandezgomez.com>
2018-05-25 19:10:10 +0200

    Karate testing framework added.

---
+++ src/test/java/core/CoreTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+public class CoreTest {
+	// this will run all *.feature files that exist in sub-directories
+	// refer to https://github.com/intuit/karate#naming-conventions
+	// for folder-structure recommendations and naming conventions
+}
---
+++ src/test/java/core/reload/ReloadRunner.java
@@ -0,0 +1,8 @@
+package core.reload;
+
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+public class ReloadRunner {
+}
